0e1695c1310e45bd0ff956098782539a4afcb959
arnav4822 <arnav4822.se25@chitkara.edu.in>
2026-08-21 15:36:30 +0530

    Add files via upload

--- sketch_aug18a_copy_20260818231927.ino
+++ sketch_aug18a_copy_20260818231927.ino
@@ -1,48 +1,123 @@
-// Pins for the lights and the button
-const int porchLight = 2;
-const int hallwayLight = 3;
-const int arrivalButton = 4;
+#include <Wire.h>
+#include <DHT.h>
+#include <BH1750.h>
+#include <WiFiNINA.h>
+#include <ThingSpeak.h>
 
-void setup() {
-  // Set the lights as outputs and the button as an input
-  pinMode(porchLight, OUTPUT);
-  pinMode(hallwayLight, OUTPUT);
-  pinMode(arrivalButton, INPUT);
+// setting pins and defining type for dht sensor
+#define DHTPIN 2
+#define DHTTYPE DHT22
 
-  Serial.begin(9600);
+DHT dht(DHTPIN, DHTTYPE);
+BH1750 lightsensor;
+
+// id and pass for my wifi
+char ssid[] = "Arnav's iphone";
+char password[] = "11221122";
+
+// channel id and api key from thing speak
+unsigned long channelID = 3464955;
+const char* writeAPIKey = "NQSNWLLGLEW57V8G";
+
+WiFiClient client;
+
+// this function helps in connecting to wifi
+void connectWiFi() {
+
+  Serial.print("Connecting to WiFi");
+
+  while (WiFi.status() != WL_CONNECTED) {
+
+    WiFi.begin(ssid, password);
+
+    delay(5000);
+
+    Serial.print(".");
+  }
+// prints connected if it works
+  Serial.println();
+  Serial.println("WiFi connected!");
 }
 
-void loop() {
-  // Check if Linda has pressed the button
-  int buttonState = digitalRead(arrivalButton);
+// funtion to read temperature
+float readTemperature() {
 
-  if (buttonState == HIGH) {
-    Serial.println("Linda has arrived! Turning on the lights...");
-    startLights();
+  float temperature = dht.readTemperature();
+  //returns the temperature
+  return temperature;
+}
+
+// function to read light level
+float readLight() {
+
+  float light = lightsensor.readLightLevel();
+  //returns the light level
+  return light;
+}
+
+// this function sends data to thing speak 
+void sendData(float temperature, float light) {
+
+  ThingSpeak.setField(1, temperature);
+  ThingSpeak.setField(2, light);
+
+  int response = ThingSpeak.writeFields(channelID, writeAPIKey);
+
+  if (response == 200) {
+    Serial.println("Data successfully sent to ThingSpeak!");
+  } 
+  else { //this else helps to tell if any problem accured while connecting
+    Serial.print("ThingSpeak error: ");
+    Serial.println(response);
   }
 }
 
-// Handles the timing for the porch and hallway lights
-void startLights() {
-  // Turn both lights on when Linda arrives
-  digitalWrite(porchLight, HIGH);
-  digitalWrite(hallwayLight, HIGH);
+// Setup code starting everything
+void setup() {
+//defining serial port
+  Serial.begin(9600);
 
-  Serial.println("Both lights are ON.");
+  dht.begin();
 
-  // Keep both lights on for 30 seconds
+  Wire.begin();
+  lightsensor.begin();
+
+  ThingSpeak.begin(client);
+
+  connectWiFi();
+
+  Serial.println("System ready!");
+}
+
+// here every funtion is called 
+void loop() {
+
+  float temperature = readTemperature();
+  float light = readLight();
+
+// this prints temp and light level in serial monitor
+  Serial.println("");
+
+  Serial.print("Temperature: ");
+  Serial.print(temperature);
+  Serial.println(" °C");
+
+  Serial.print("Light: ");
+  Serial.print(light);
+  Serial.println(" lux");
+
+// exeption handelling
+  if (isnan(temperature)) {
+
+    Serial.println("Temperature reading failed!");
+
+  } 
+  else {
+
+    sendData(temperature, light);
+  }
+
+  Serial.println("Wait 30 seconds");
+
   delay(30000);
-
-  // Turn the porch light off after 30 seconds
-  digitalWrite(porchLight, LOW);
-
-  Serial.println("Porch light is OFF.");
-
-  // Keep the hallway light on for another 30 seconds
-  delay(30000);
-
-  // Turn the hallway light off after one minute
-  digitalWrite(hallwayLight, LOW);
-
-  Serial.println("Hallway light is OFF. Done.");
 }
