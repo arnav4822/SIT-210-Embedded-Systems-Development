c8a53fefef1bdde580e5ef28b9de67ddb4dfe302
arnav4822 <arnav4822.se25@chitkara.edu.in>
2026-08-29 16:52:25 +0530

    Add files via upload

--- sketch_aug18a_copy_20260818231927.ino
+++ sketch_aug18a_copy_20260818231927.ino
@@ -1,123 +1,150 @@
 #include <Wire.h>
-#include <DHT.h>
 #include <BH1750.h>
 #include <WiFiNINA.h>
-#include <ThingSpeak.h>
+#include <ArduinoMqttClient.h>
 
-// setting pins and defining type for dht sensor
-#define DHTPIN 2
-#define DHTTYPE DHT22
+BH1750 lightSensor;
 
-DHT dht(DHTPIN, DHTTYPE);
-BH1750 lightsensor;
+char ssid[] = "TT";
+char password[] = "12345678";
 
-// id and pass for my wifi
-char ssid[] = "Arnav's iphone";
-char password[] = "11221122";
+const char mqttServer[] =
+  "8abfc976a0b944208bac1582ad0cb531.s1.eu.hivemq.cloud";
 
-// channel id and api key from thing speak
-unsigned long channelID = 3464955;
-const char* writeAPIKey = "NQSNWLLGLEW57V8G";
+const int mqttPort = 8883;
 
-WiFiClient client;
+const char mqttUsername[] = "arnav_mqtt";
+const char mqttPassword[] = "ArnaV@2006";
 
-// this function helps in connecting to wifi
-void connectWiFi() {
+const char mqttTopic[] = "arnav/sensors";
 
-  Serial.print("Connecting to WiFi");
+WiFiSSLClient wifiClient;
+MqttClient mqttClient(wifiClient);
 
-  while (WiFi.status() != WL_CONNECTED) {
+void connectWiFi()
+{
+  Serial.print("Connecting to Wi-Fi");
 
+  while (WiFi.status() != WL_CONNECTED)
+  {
     WiFi.begin(ssid, password);
 
     delay(5000);
 
     Serial.print(".");
   }
-// prints connected if it works
+
   Serial.println();
-  Serial.println("WiFi connected!");
+  Serial.println("Wi-Fi connected!");
+
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
 }
 
-// funtion to read temperature
-float readTemperature() {
+void connectMQTT()
+{
+  Serial.print("Connecting to HiveMQ");
 
-  float temperature = dht.readTemperature();
-  //returns the temperature
-  return temperature;
-}
+  mqttClient.setUsernamePassword(
+    mqttUsername,
+    mqttPassword
+  );
 
-// function to read light level
-float readLight() {
-
-  float light = lightsensor.readLightLevel();
-  //returns the light level
-  return light;
-}
-
-// this function sends data to thing speak 
-void sendData(float temperature, float light) {
-
-  ThingSpeak.setField(1, temperature);
-  ThingSpeak.setField(2, light);
-
-  int response = ThingSpeak.writeFields(channelID, writeAPIKey);
-
-  if (response == 200) {
-    Serial.println("Data successfully sent to ThingSpeak!");
-  } 
-  else { //this else helps to tell if any problem accured while connecting
-    Serial.print("ThingSpeak error: ");
-    Serial.println(response);
+  while (!mqttClient.connect(mqttServer, mqttPort))
+  {
+    Serial.print(".");
+    delay(5000);
   }
+
+  Serial.println();
+  Serial.println("Connected to HiveMQ!");
 }
 
-// Setup code starting everything
-void setup() {
-//defining serial port
+void setup()
+{
   Serial.begin(9600);
 
-  dht.begin();
+  delay(2000);
 
+  Serial.println();
+
+  Serial.println("Light Sensor System Starting");
+
+
+  
   Wire.begin();
-  lightsensor.begin();
 
-  ThingSpeak.begin(client);
+  Serial.println("I2C started");
+
+  if (lightSensor.begin())
+  {
+    Serial.println("BH1750 sensor ready!");
+  }
+  else
+  {
+    Serial.println("BH1750 sensor failed!");
+  }
 
   connectWiFi();
 
+  connectMQTT();
+
+  Serial.println();
   Serial.println("System ready!");
 }
 
-// here every funtion is called 
-void loop() {
-
-  float temperature = readTemperature();
-  float light = readLight();
-
-// this prints temp and light level in serial monitor
-  Serial.println("");
-
-  Serial.print("Temperature: ");
-  Serial.print(temperature);
-  Serial.println(" °C");
-
-  Serial.print("Light: ");
-  Serial.print(light);
-  Serial.println(" lux");
-
-// exeption handelling
-  if (isnan(temperature)) {
-
-    Serial.println("Temperature reading failed!");
-
-  } 
-  else {
-
-    sendData(temperature, light);
+void loop()
+{
+  
+  if (!mqttClient.connected())
+  {
+    connectMQTT();
   }
 
-  Serial.println("Wait 30 seconds");
+  mqttClient.poll();
 
-  delay(30000);
+  float light = lightSensor.readLightLevel();
+
+  if (light < 0)
+  {
+    Serial.println("Light reading failed!");
+
+    delay(5000);
+
+    return;
+  }
+
+  Serial.println();
+
+
+  Serial.print("Light: ");
+  Serial.print(light, 2);
+  Serial.println(" lux");
+
+  String message = "{";
+
+  message += "\"light\":";
+  message += String(light, 2);
+
+  message += "}";
+
+  Serial.print("MQTT message: ");
+  Serial.println(message);
+
+  mqttClient.beginMessage(mqttTopic);
+
+  mqttClient.print(message);
+
+  mqttClient.endMessage();
+
+
+  Serial.println("Data sent to HiveMQ!");
+
+  for (int i = 0; i < 30; i++)
+  {
+    mqttClient.poll();
+    delay(1000);
+  }
 }
+
+
